f3ddaec8c6f78f48a8465db4b60eb70dbbaf71a7
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-07-11 00:05:15 +0000

    update server

--- API/server.js
+++ API/server.js
@@ -1,1 +1,13 @@
-console.log('hello world')
+const express = require('express');
+const colors = require('colors');
+const dotenv = require('dotenv').config();
+
+const port = process.env.PORT || 5000;
+
+const app = express()
+
+app.get('/api/jetski', (req, res) => {
+    res.json({message: "Get All Jetski"})
+})
+
+app.listen(port, () => console.log(`server started on port ${port}`))
