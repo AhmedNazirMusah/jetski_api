770ffa5f6eae1edeaba9e4a69a7f2ce237af7d58
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-07-16 00:07:09 +0000

    update the routes with authentication

--- API/routes/jetskiRoutes.js
+++ API/routes/jetskiRoutes.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { getJetskis,setJetski,deleteJetski, updateJetski } = require('../controllers/jetskiController')
-const { guard } = require('../middleware/authMiddleware')  
+const { guard, protect } = require('../middleware/authMiddleware')  
 
-router.route('/').get( getJetskis ).post(guard, setJetski)
+router.route('/').get(protect, getJetskis ).post(guard, setJetski)
 router.route('/:id').put(guard, updateJetski).delete(guard, deleteJetski)
 
 module.exports = router
