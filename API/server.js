2528df2f6227d8e7ae8e79de4b9ec903a62ebcaf
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-07-16 00:06:35 +0000

    update server,js will correct routing

--- API/server.js
+++ API/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const colors = require('colors');
 const dotenv = require('dotenv').config();
 const { errorHandler } = require('./middleware/errorMiddleware');
+const adminRoutes = require('./routes/adminRoutes');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
@@ -11,9 +12,8 @@ const app = express()
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
 app.use('/api/users', require('./routes/userRoutes'));
-app.use('./api/admin', require('./routes/adminRoutes'))
-app.use('./api/jetski', require('./routes/jetskiRoutes'))
+app.use('/api/admin', adminRoutes);
+app.use('/api/jetski', require('./routes/jetskiRoutes'));
 app.use(errorHandler);
-app.listen(port, () => console.log(`server started on port ${port}`))
+app.listen(port, () => console.log(`server started on port ${port}`));
