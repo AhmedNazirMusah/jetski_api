70d31a7c6df072ae40febabc0c51f623c867720a
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-07-18 23:44:33 +0000

    update

--- API/server.js
+++ API/server.js
@@ -5,6 +5,7 @@ const { errorHandler } = require('./middleware/errorMiddleware');
 const adminRoutes = require('./routes/adminRoutes');
 const reservationRoutes = require('./routes/reservationRoutes');
 const userRoutes = require('./routes/userRoutes');
+const jetskiRoutes = require('./routes/jetskiRoutes');
 const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 
@@ -17,6 +18,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use('/api/users', userRoutes);
 app.use('/api/admin', adminRoutes);
 app.use('/api/reservations', reservationRoutes);
-app.use('/api/jetski', require('./routes/jetskiRoutes'));
+app.use('/api/jetski', jetskiRoutes);
 app.use(errorHandler);
 app.listen(port, () => console.log(`server started on port ${port}`));
