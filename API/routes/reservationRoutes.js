22e61ebe09185f3a2c46d04d7d026d87b5b9dc11
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-07-18 23:24:44 +0000

    update routes

--- API/routes/reservationRoutes.js
+++ API/routes/reservationRoutes.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const router = express.Router()
-const { getReservations, addReservation } = require('../controllers/reservationController')
+const { getReservations, addReservation, deleteReservation } = require('../controllers/reservationController')
 const { protect } = require('../middleware/authMiddleware')
 
 router.route('/').get(protect, getReservations ).post(protect, addReservation)
-
+router.route('/:id').delete(protect, deleteReservation)
 module.exports = router
