f1c15145b71330de4dfaf540d258ed3544ac23ca
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-07-18 23:23:41 +0000

    Update Aggregation for reserved jetskis

--- API/controllers/reservationController.js
+++ API/controllers/reservationController.js
@@ -7,12 +7,19 @@ const Reservation = require('../models/reservationModel')
 // @access  Private
 const getReservations = asyncHandler(async (req, res) => {
     const reservations = await Reservation.find({ user: req.user.id }).populate('jetskis');
-  
-       // Aggregate jetskis from all reservations
-        const jetskis = reservations.reduce((acc, reservation) => {
-          acc.push(...reservation.jetskis);
-          return acc;
-      }, []);
+    
+    // Aggregate jetskis from all reservations
+
+    const jetskis = reservations.reduce((acc, reservation) => {
+      reservation.jetskis.forEach(jetski => {
+        acc.push({
+          jetski,
+          starting_date: reservation.startDate,
+          end_date: reservation.endDate,
+        });
+      });
+      return acc;
+    }, []);
   
       res.status(200).json(jetskis);
   })
@@ -33,9 +40,42 @@ const addReservation = asyncHandler(async (req, res) => {
     res.status(200).json(reservations)
   })
 
+  const deleteReservation = asyncHandler(async (req, res) => {
+    console.log(`Received request to delete reservation with ID: ${req.params.id}`);
+    const reservation = await Reservation.findById(req.params.id)
+    
+    if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+      res.status(400);
+      throw new Error('Invalid reservation ID');
+    }
+
+    if (!reservation) {
+      res.status(400)
+      throw new Error('Reservation not found')
+    }
+  
+    // Check for user
+  
+    if (!req.user) {
+      res.status(401)
+      throw new Error('User not found')
+    }
+  
+    // Make sure the logged in is the admin
+    if (reservation.user.toString() !== req.user.id) {
+      res.status(401)
+      throw new Error('User not authorized')
+    }
+  
+    await Reservation.deleteOne({ _id: req.params.id });
+  
+    res.status(200).json({ id: req.params.id })
+  })
+
 //update to add update and delete reservation
   
 module.exports = {
     getReservations,
     addReservation,
+    deleteReservation,
 }
