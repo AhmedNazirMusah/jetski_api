7f8c049d34355bdf2aa3c547334ef2a3406dade3
ahmednazirmusah <ahmed999nazir@gmail.com>
2024-07-24 22:48:18 +0000

    update routes for jetski

--- API/routes/jetskiRoutes.js
+++ API/routes/jetskiRoutes.js
@@ -2,8 +2,8 @@ const express = require('express')
 const router = express.Router()
 const { getJetskis,setJetski,deleteJetski, updateJetski } = require('../controllers/jetskiController')
 const { guard, protect } = require('../middleware/authMiddleware')  
-
-router.route('/').get(protect, getJetskis ).post(guard, setJetski)
+const upload = require('../middleware/upload')
+router.route('/').get(protect, getJetskis ).post(guard,upload.single('image'), setJetski)
 router.route('/:id').put(guard, updateJetski).delete(guard, deleteJetski)
 
 module.exports = router
